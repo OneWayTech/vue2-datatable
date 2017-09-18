0af2add593e464825736a2a6d843753150b52508
Ken Berkeley <kenberkeley@foxmail.com>
2017-09-18 12:06:29 +0800

    [fix #10]

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-const Datatable = {
-  install (Vue, options) {
-    const locale = options && options.locale || {}
-    
-    // this might be the simplest i18n solution
-    Vue.prototype.$i18nForDatatable = srcTxt => locale[srcTxt] || srcTxt
+import Datatable from './Datatable.vue'
 
-    Vue.component('Datatable', require('./Datatable.vue'))
-  }
+Datatable.install = (Vue, options) => {
+  const locale = options && options.locale || {}
+  
+  // this might be the simplest i18n solution
+  Vue.prototype.$i18nForDatatable = srcTxt => locale[srcTxt] || srcTxt
+
+  Vue.component('Datatable', Datatable)
 }
 
 if (typeof window !== 'undefined' && window.Vue) {

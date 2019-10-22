a203f19f1cd2a56ef325e1e6439cda8c06a3753c
Paolo Battistella <p.battistella@egeatech.com>
2019-10-22 19:23:38 +0200

    Add italian translation

---
+++ locale/it-it.js
@@ -0,0 +1,17 @@
+export default {
+  /* HeaderSettings/index.vue */
+  'Apply': 'Applica',
+  'Apply and backup settings to local': 'Applica e salva nelle impostazioni locali',
+  'Clear local settings backup and restore': 'Cancella le impostazioni locali salvate e ripristina',
+  'Using local settings': 'Utilizza le impostazioni locali',
+
+  /* Table/TableBody.vue */
+  'No Data': 'Nessun Dato',
+
+  /* index.vue */
+  'Total': 'Totale',
+  ',': ',',
+
+  /* PageSizeSelect.vue */
+  'items / page': 'elementi / pagina'
+}
